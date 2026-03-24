d4e8d1206f695bc90694d78409d319aff3056211
kjminn <kjminn2001@gmail.com>
2026-03-24 11:50:18 +0900

    4장 Controller 연습

---
+++ src/main/java/kr/ac/kopo/minn/_026example/controller/EX06Controller.java
@@ -0,0 +1,4 @@
+package kr.ac.kopo.minn._026example.controller;
+
+public class EX06Controller {
+}
---
+++ src/test/java/kr/ac/kopo/minn/_026example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.minn._026example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
