150da4fbfdcbff1e554c9d6495c218f797e7c3a7
kjminn <kjminn2001@gmail.com>
2026-05-19 11:47:53 +0900

    9장 2. CostraintValidator 구현체 예제

---
+++ src/main/java/kr/ac/kopo/minn/_026example/domain/Member2.java
@@ -0,0 +1,14 @@
+package kr.ac.kopo.minn._026example.domain;
+
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class Member2 {
+    @MemberId
+    private String memberId;
+
+    @Size(min=4, max = 10, message = "최소 4 ~ 최대 10개의 문자열로 작성해야합니다.")
+    private String passwd;
+
+}
