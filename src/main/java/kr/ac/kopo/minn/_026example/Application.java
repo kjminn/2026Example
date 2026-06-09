1b457e3af36666ecbf9c918abcb43e0206ffd615
kjminn <kjminn2001@gmail.com>
2026-06-09 11:42:48 +0900

    사용자 정보 검색 유형 중에 Authentication 유형을 사용함

--- src/main/java/kr/ac/kopo/minn/_026example/Application.java
+++ src/main/java/kr/ac/kopo/minn/_026example/Application.java
@@ -1,5 +1,6 @@
 package kr.ac.kopo.minn._026example;
 
+import kr.ac.kopo.minn._026example.configure.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/kr/ac/kopo/minn/_026example/controller/Ex10Controller.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.kopo.minn._026example.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class Ex10Controller {
-}
