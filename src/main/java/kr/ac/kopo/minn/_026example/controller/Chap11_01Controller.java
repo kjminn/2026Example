49de1bb1cf5e5a2dcf2802b18e22c9e7be2d4ab0
kjminn <kjminn2001@gmail.com>
2026-08-18 10:47:31 +0900

    @ExceptionHandler를 사용한 예외처리 방법

--- src/main/java/kr/ac/kopo/minn/_026example/controller/Chap11_01Controller.java
+++ src/main/java/kr/ac/kopo/minn/_026example/controller/Chap11_01Controller.java
@@ -3,6 +3,8 @@ package kr.ac.kopo.minn._026example.controller;
 import kr.ac.kopo.minn._026example.exception.UserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,5 +21,22 @@ public class Chap11_01Controller {
     @GetMapping("/exam02")
     public void requestMethod02() throws Exception{
         throw new Exception(new UserException("UserException 메시지입니다."));
+    }
+
+    @GetMapping("/exam03")
+    public void requestMethod03(){
+        throw new UserException("UserException @ExceptionHandler 예제 메시지입니다.");
+    }
+
+    @ExceptionHandler(UserException.class)
+    public String handlerException(UserException ex, Model model){
+        model.addAttribute("data1", ex.getMessage());
+        model.addAttribute("data2", ex.toString());
+        return "viewPage11_01";
+    }
+
+    @GetMapping("/exam04")
+    public void requestMethod04(){
+        throw new UserException("UserException @ControllerAdvice 예제 메시지입니다.");
     }
 }
